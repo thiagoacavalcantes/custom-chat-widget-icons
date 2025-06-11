f5b5e38217294fbbb9a66e9cd95aed187a014234
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2025-06-11 17:20:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const messages = [
 
 function getPlatformIcon(platform) {
   if (platform === 'Twitch') {
-    return '<img src="https://imgur.com/sncEWQk" alt="Twitch" class="platform-icon">';
+    return '<img src="https://jmp.sh/BNSad9Qx" alt="Twitch" class="platform-icon">';
   } else if (platform === 'YouTube') {
-    return '<img src="https://imgur.com/Kn21DSD" alt="YouTube" class="platform-icon">';
+    return '<img src="https://jmp.sh/knoAqL0p" alt="YouTube" class="platform-icon">';
   }
   return '';
 }
